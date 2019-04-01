39944fccdf829f1797f7441f252eca421972743c
basim1979 <auguste.ba@lepoles.com>
2019-04-01 17:05:13 +0200

    01/04/2019 PHP

--- COURS PHP/01_entrainement.php
+++ COURS PHP/01_entrainement.php
@@ -380,7 +380,243 @@ function affichagePays()
 echo affichagePays();
 
 
+echo '<hr><h2 class="display-4 text-center">STRUCTURE ITERATIVE: BOUCLE</h2>';
+
+//Boucle while
+
+$i = 0;
+while($i < 5)
+{
+    echo "$i---";
+    echo '<br>';
+    $i++; // équivaut à $i + 1
+
+}
+
+//EXO de ne pas avoir de tirets à la fin
+
+$j = 0;
+while($j < 5)
+{
+    if($i !== 4)
+    echo "$j---";
+    else
+    echo "$j";
+    $j++;
+echo '<br>';
+}
+//----------------------------------------------------------------------
+
+//Boucle FOR
+
+for($j = 0; $j < 16; $j++) //initialisation /codition d'entrée incrementation
+{
+  echo "$j <br>";
+  
+}
+   
+   
+   
+//EXO : afficher 30 options via une boucle
+echo '<hr><select></select>';
+for($i = 0; $i<31; $i++)
+{
+    echo "<option>$i</option>";
+}
+echo "</select>";
+//Exo: Faites une boucle qui affiche de 0 à 9 sur la même ligne (soit 10 tours)
+
+for($d = 0; $d < 10; $d++)
+{
+    echo $d;
+}
+
+//Exo; faites une boucle qui affiche de 0 à 9 sur la même ligne dans un tableau HTML
+
+/*  
+-----------------------
+|0 |1|2|3|4 |5|6|7|8|9| 
+-----------------------
+*/
+
+echo '<table class="table table-bordered text-center"><tr>';
+for($v = 0; $v < 10; $v++)
+{
+    echo "<td>$v</td>"; //on c'rée une option par tour de boucle avec la valeur de $v dans chaque cellule
+
+}
+echo '</tr></table> <hr>';
+
+
+//EXERCICE: INFO boucle imbriquée
+//FOR
+
+/* 
+---------------------------------
+|0 |  1|  2|3| 4 |5 |6|7|8 | 9 | 
+---------------------------------
+|10 |11|12|13|14 |15|16|17|18|19| 
+---------------------------------
+|20 |21|22|23|24 |25|26|27|28|29| 
+---------------------------------
+|30 |31|32|33|34 |35|36|37|38|39| 
+---------------------------------
+|90 |91|92|93|94 |95|96|97|98|99| 
+---------------------------------
+*/
+$compteur = 0;
+//La premiere boucle FOR tourne 10 fois parce qu'il y a 10 lignes
+//La deuxieme boucle FOR tourne 10 fois sur chaque ligne et crée une cellule.
+//$compteur permet d'avoir une variable qui ne se reinitialise jamais à zero, elle augmente de 1 quelque soit 
+echo '<table class="table table-bordered text-center">';
+for($ligne = 0; $ligne < 10; $ligne++)
+{
+    echo '<tr>'; //on c'rée une option par tour de boucle avec la valeur de $v dans chaque cellule
+    for($cellule = 0; $cellule < 10; $cellule++)
+    {
+        echo "<td>$compteur</td>";
+        $compteur++;
+    }
+    echo'</tr>';
+}
+echo '</table>';
+
+echo '<hr><h2 class="display-4 text-center">Tableau de données ARRAY</h2>';
+
+//Unn tableau array est declarée un peu comme une variable ameiorée, car on ne conservepas qu'une valeur 
+
+$liste = array("Gregory", "Aziz", "Nassim", "Sylvain", "Nelson");
+$liste = ["Gregory", "Aziz", "Nassim", "Sylvain", "Nelson"];
+
+// echo $liste; /!\ erreur!! on ne peut pas afficher un tableau ARRAY
+echo '<pre>'; var_dump($liste); echo '</pre>';//Plus detaillé
+echo '<pre>'; print_r($liste); echo '</pre>';//Moins detaillé
+//Pré est une balise qui permet de formater la sortie du pront_r ou var_dump
+//Ces instructions d'affichages ameliorées permettent de consulter et d'afficher les données d'un tableau, d'une variable, d'un objet etc...
+//deux instructions d'affichage ameliorées
+//Indice associé à une valeur => c'est le princvipe du PHP
+
+/* 
+Exo: Tenter de sortir "Aziz" en passant par le tableau par de donnée ARRAY sans faire 'echo Aziz'*/
+echo $liste[1] . '<br>';// On va crocheter
+
+
+echo '<hr><h2 class="display-4 text-center">Les tableaux de données ARRAY</h2>';
+
+$tab[] = "France";// Autre moyen d'afficher une valeur dans un tableau, les crochets vides permettent de generer des indices numeriques
+$tab[] = "Angleterre";
+$tab[] = "Espagne";
+$tab[] = "Italie";
+$tab[] = "Portugal";
+
+// echo $tab; :!\ erreur 
+echo '<pre>'; print_r($tab); echo '</pre>';//Moins detaillé
+
+//Une boucle qui va parcourir tout le tableau ARRAY si on a 500 données sans chercher à se repeter boucle "Foreach"
+/* 
+-----------------------------------
+|       Indice        |  Valeur   |
+-----------------------------------
+-----------------------------------
+|       0        |     France     |
+-----------------------------------
+-----------------------------------
+|       1        |Angleterre      |
+-----------------------------------
+-----------------------------------
+|       2        | Espagne        |
+-----------------------------------
+-----------------------------------
+|       2        | Italie         |
+-----------------------------------
+-----------------------------------
+|       2        | Portugal       |
+-----------------------------------
+
+
+*/
+
+
+//Lorsqu'il n'y a qu'une seule variable, $value parcours la colonne des valeurs du tableau de données ARRAY
+//La boucle foreach est un moyen simple de passer en revue un tableau de données ARRAY(aussi les objets : futurs chapitre)
+foreach($tab as $value) //as fait partie du langage et est obligatoire , $vbalue est une variable de reception que nous nommons , elle receptionne une valeurdu tableau par tour de boucle.
+{
+  echo "$value <br>"; //on, affiche successivement les éléments du tableau
+}
+echo '<br>';
+//----------------------------------------------------------------------------------------------
+
+//Foreach: indice + valeur
+foreach($tab as $key => $value)// La flêche est obligatoire
+{
+    echo "$key => $value <br>"; //key parcours les indices et l'aure parcours les valeurs
+}
+   
+//Une autre formulation
    ?>
+   <!--2ème possibilité d'écriture-->
+   <hr>
+   <?php foreach($tab as $key => $value): ?>
+   <?= $key; ?> => <?= $value; ?> <br>
+<?php endforeach; ?>
+<!--for(): / endfor-->
+<!--if(): / else: / endif-->
+<!--while(): endwhile/ endfor-->
+<?php
+ $perso = array("m" => "Mario", "1" => "Luigi", "a" => "Aziz", "n" => "Nassim");
+ echo '<pr>'; print_r($perso); echo '<br>';
+ echo "taille du tableau: ". count($perso). '<br>';
+ echo "taille du tableau: ". sizeof($perso). '<br>';
+//sizeof et count retourne la taille d'unn tableau ARRAY , combien d'éléments 
+
+echo implode("/", $perso) . '<br>'; // Imploed() est une 
+
+echo '<hr><h2 class="display-4 text-center">LES TABLEAU MULTIDIMENSIONNELS</h2>';
+
+// Nous parlons de tableau multidimensionnel quand un tableau est conternu dans un autre tableau
+$tab_multi = array(
+0 => array("nom" => "macron", "salaire" => 1),
+1 =>array("nom" => "Lacroix", "salaire"=> 15000)
+
+
+);
+echo '<pre>'; print_r($tab_multi); echo '</pre>';
+
+//Exo: Tenter de sortir "macron" en pensant par le tableau multidimensionnel reprsenté par $tab_multi sans faire 'echo Macron
+
+echo $tab_multi[0]['nom'] .'<br>';
+
+
+
+
+//Afficher  l'ensemble du tableau multidimensionnel à l'aide d'une boucle foreach
+foreach($tab_multi as $key => $tab)
+{
+    echo '<div class="col-md-3 offset-5 alert-success text-dark mx-auto text-center">';
+    foreach($tab as $key2 => $value)
+    {
+        echo "$key2 => $value <br>";
+    }
+    echo '</div>';
+    
+}
+echo '<hr>';
+    //------------------------------------------------------------------------------------
+    for($i = 0; $i < count($tab_multi); $i++)
+    {
+        echo '<div class="col-md-2 offset-md-6 alert-info text-dark mx-auto text-center">';
+        foreach($tab_multi[$i] as $key => $value)
+        {
+            echo "$key => $value<br>";
+        }
+        echo '</div>';
+    }   
+
+
+   
+
+?>
+   
    </div>
    <!--Biblioteque pour Boostrap-->
    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
---
+++ COURS PHP/02-compartiment -site-include/include/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Mon site de la mort</title>
+</head>
+<body>
+
+<div class="container" >
+<header class="bg-secondary p-4 d-flex">
+<img src="https://static.fnac-static.com/multimedia/Images/FR/NR/fb/c3/81/8504315/1540-1/tsp20170119134647/Ordi-Genius-Kid-Vtech.jpg" alt="" style="height: 150px; width: 150px" class="m-2">
+<h1>MON SITE DE LA MORT</h1>
+</header>
